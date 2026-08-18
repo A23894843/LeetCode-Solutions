6c28d53bc6dc4b7272a890d945b39ec008c94744
Abhinandan <a23894843@gmail.com>
2026-08-18 23:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (45.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -10,13 +10,25 @@ public:
             product *= i;
         }
 
-        if (zero == 1)  {
-            for (int i = 0; i < nums.size(); i++)   {
-                result[i] = (nums[i] == 0) ? product : 0;
+        for (int i = 0; i < nums.size(); i++)   {
+            if (zero == 1)  {
+                if (nums[i] == 0)   {
+                    result[i] = product;
+                }   else    {
+                    result[i] = 0;
+                }   
+            }   else if (zero == 0) {
+                result[i] = product / nums[i];
             }
-        }   else if (zero == 0) {
-            for (int i = 0; i < nums.size(); i++)  result[i] = product / nums[i];
         }
+
+        // if (zero == 1)  {
+        //     for (int i = 0; i < nums.size(); i++)   {
+        //         result[i] = (nums[i] == 0) ? product : 0;
+        //     }
+        // }   else if (zero == 0) {
+        //     for (int i = 0; i < nums.size(); i++)  result[i] = product / nums[i];
+        // }
        return result;
     }
 };
